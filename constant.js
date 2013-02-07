7a8d277985deded8c604b63f40f0245382d94040
Vianney Lecroart <vianney@dok.io>
2013-02-07 11:54:31 +0100

    new use case

--- constant.js
+++ constant.js
@@ -1,44 +1,46 @@
 Blocks = new Meteor.Collection("blocks");
- 
-function saveBlock() {
-	var block = Blocks.findOne({});
-	var o = block.text;
-	var n = $('.editor').html();
-	if(o != n) {
-		Blocks.update(block._id, { $set: { text: n } });
-		console.log("save");
-	}
-}
- 
+
 if (Meteor.isClient) {
- 
-	Session.set('blocksLoading', true);
-  
-	Meteor.subscribe('blocks', function() {
-			console.log('block subs completed');
-			Session.set('blocksLoading', false);
-		});
- 
-	Template.blocks.blocksLoading = function () {
-		return Session.equals('blocksLoading', true);
-	};
- 
-	Template.block.random = function () {
-		return Math.random();
-	};
- 
-	Template.blocks.blocks = function () {
-		return Blocks.find();
-	};
- 
-	Meteor.setInterval(function() { saveBlock(); }, 1000);
+
+  Session.set('blocksLoading', true);
+  Meteor.subscribe('blocks', function() {
+    console.log('blocks received');
+    Session.set('blocksLoading', false);
+  });
+
+  Template.block.created = function () {
+    this.rndval = Math.random();
+    console.log("created", this.rndval);
+  };
+
+  Template.block.destroyed = function () {
+    console.log("destroyed", this.rndval);
+  };
+
+  Template.block.rendered = function () {
+    console.log("rendered", this.rndval);
+  };
+
+  Template.main.blocksLoading = function () {
+    return Session.equals('blocksLoading', true);
+  };
+
+  Template.blocks.blocks = function () {
+    return Blocks.find();
+  };
+
+  Template.blocks.events({
+    'click button': function () {
+      Blocks.update(Blocks.findOne()._id, { $set: { foo: Math.random() } });
+    }
+  });
 }
  
 if (Meteor.isServer) {
-	if(Blocks.find().count() == 0)
-		Blocks.insert({text: "edit me"});
+  if(Blocks.find().count() == 0)
+    Blocks.insert({text: "edit me"});
   
-	Meteor.publish("blocks", function (s) {
-			return Blocks.find();
-		});
+  Meteor.publish("blocks", function (s) {
+    return Blocks.find();
+  });
 }
